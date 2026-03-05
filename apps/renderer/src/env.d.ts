16007318d669ea89bc7502ae06f0afa18015c3c1
miyaoka <miyaokamiyao@gmail.com>
2026-03-06 01:40:37 +0900

    remove env.d.ts and consolidate types into tsconfig

--- apps/renderer/src/env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-/// <reference types="@orkis/preload" />
-
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
-  const component: DefineComponent;
-  export default component;
-}

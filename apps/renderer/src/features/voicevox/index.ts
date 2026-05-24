6a085b959dcef7ab2362154c85652765779735f0
miyaoka <miyaokamiyao@gmail.com>
2026-05-24 12:09:15 +0900

    chore(voicevox): tidy whitespace in barrel after knip auto-removal

--- apps/renderer/src/features/voicevox/index.ts
+++ apps/renderer/src/features/voicevox/index.ts
@@ -1,3 +1,3 @@
 export { extractAskingText, extractFirstSentence } from "./speechText";
-export {  useVoicevoxStore } from "./useVoicevoxStore";
+export { useVoicevoxStore } from "./useVoicevoxStore";
 export { default as VoicevoxSpeakerSelect } from "./VoicevoxSpeakerSelect.vue";
